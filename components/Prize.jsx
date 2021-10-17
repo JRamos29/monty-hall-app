590ed54a22c95a232a73ca548183a183684b1405
JRamos29 <malebolge@gmail.com>
2021-10-17 18:48:59 -0300

    Initial commit.

---
+++ components/Prize.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Prize.module.css';
+
+export default function Prize() {
+	return (
+		<div className={styles.prize}>
+			<div className={styles.top}></div>
+			<div className={styles.body}></div>
+			<div className={styles.ribbon1}></div>
+			<div className={styles.ribbon2}></div>
+		</div>
+	);
+}
