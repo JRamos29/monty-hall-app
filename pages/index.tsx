a79810445b196036d496cbf6b8de72391c583119
JRamos29 <malebolge@gmail.com>
2021-10-17 22:21:38 -0300

    Initial implementation for game start page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,9 @@ import { useState } from 'react';
 import { createDoors, updateDoors } from '../functions/doors';
 
 export default function Home() {
-	const [doors, setDoors] = useState(createDoors(4, 3));
-
-	console.log(createDoors(3, 2));
-
-	function renderDoors() {
-		return doors.map((door) => {
-			return (
-				<Door
-					key={door.number}
-					value={door}
-					onChange={(newDoor) =>
-						setDoors(updateDoors(doors, newDoor))
-					}
-				/>
-			);
-		});
-	}
-
-	return <div style={{ display: 'flex' }}>{renderDoors()}</div>;
+	return (
+		<div>
+			<h1>Game Start Panel</h1>
+		</div>
+	);
 }
