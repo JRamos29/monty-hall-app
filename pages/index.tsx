5d51c93122619b1281925cc806e68a65c206d197
JRamos29 <malebolge@gmail.com>
2021-10-17 23:08:37 -0300

    Structuring the game start page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
-import Door from '../components/Door';
-import DoorModel from '../model/door';
-import { useState } from 'react';
-import { createDoors, updateDoors } from '../functions/doors';
+import styles from '../styles/Form.module.css';
+import Card from '../components/Card';
+import Link from 'next/link';
 
-export default function Home() {
+export default function Form() {
 	return (
-		<div>
-			<h1>Game Start Panel</h1>
+		<div className={styles.form}>
+			<div>
+				<Card bgcolor="#c0392c">
+					<h1>Monty Hall</h1>
+				</Card>
+				<Card></Card>
+			</div>
+			<div>
+				<Card></Card>
+
+				<Card bgcolor="#28a085">
+					<Link href={`/game/4/2`}>
+						<h2 className={styles.link}>Start</h2>
+					</Link>
+				</Card>
+			</div>
 		</div>
 	);
 }
